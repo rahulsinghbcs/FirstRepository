e2de411453514c55815990baa778697b22f0a5b6
rahulsinghbcs <rahul.singh@blackcrowsys.com>
2018-03-28 09:04:53 +0530

    First commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FirstTest {
+
+    @Test
+    public void firstTest() {
+        assertEquals(1+1, 2);
+    }
+}
